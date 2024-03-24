e725b2d24c95e7f47d311985c1991c296b3c35a9
Evgenii Vindiukov <halltape@Evgeniis-MacBook-Pro.local>
2024-03-24 21:04:22 +0300

    first_commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Halltape ETL [Docker Compose]
+
+## Getting Started
+
+### Building the Docker Images
+
+To build the Docker images, navigate to the root directory of the project in your terminal and run the following command in background:
+```bash
+docker-compose up -d
+```
+
+To show all the running docker containers
+```bash
+docker ps
+```
+
+To stop all the docker containers
+```bash
+docker-compose down
+```
+
+To go inside the docker container
+```bash
+docker exec -it <containerID> bash
+```
+
+This project sets up a Docker environment with Airflow, PostgreSQL, and ClickHouse. The services are orchestrated using Docker Compose.
+
+***
+## Prerequisites
+
+- Docker: Make sure you have Docker installed on your system. You can download it from [here](https://www.docker.com/products/docker-desktop).
+- Docker Compose: Also ensure that Docker Compose is installed. You can find it [here](https://docs.docker.com/compose/install/).
+
+## Services
+
+| Service | Port | User    | Password |
+|---------|------|---------|----------|
+| Airflow | http://localhost:8080 |   airflow      |    airflow      |
+| PostgreSQL | http://localhost:5432 | airflow | airflow         |
+| ClickHouse | http://localhost:9000 |  airflow       |   airflow       |
+
+
+### PostgreSQL
+
+The PostgreSQL service is used as the backend database for Airflow. The service uses the `postgres:15` image and exposes the default PostgreSQL port `5432`. The data for the service is persisted in the `postgres-db-volume` volume.
+
+### ClickHouse
+
+The ClickHouse service is used for data storage and querying. The service uses the `yandex/clickhouse-server:latest` image and exposes the default ClickHouse ports `8123` and `9000`. The data for the service is persisted in the `./clickhouse_data` and `./clickhouse_logs` volumes.
+
+### Airflow
+
+The Airflow service is split into three separate services for the webserver, scheduler, and worker. Each service is built from the `Dockerfile` in the `./airflow_docker_image` directory. The services share common environment variables and volumes defined in the `x-airflow-common` section.
---
+++ dags/extract_data.py
@@ -0,0 +1,90 @@
+from airflow import DAG
+from clickhouse_driver import Client
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+import pandas as pd
+import requests
+import json
+
+
+client = Client(host='clickhouse', port=9000,
+                user='airflow', password='airflow')
+
+dag = DAG(
+    dag_id="halltape_etl",
+    start_date=datetime(2024,3,9),
+    schedule_interval="@daily"
+)
+
+
+def get_data_from_api():
+    url = 'https://api.spacexdata.com/v3/launches'
+    response = requests.get(url)
+    launches_json = response.json()
+
+    with open('/opt/airflow/dags/data/launches.json', 'w') as write:
+        json.dump(launches_json, write)
+
+    data = {
+        'Name': ['Alice', 'Bob', 'Charlie'],
+        'Age': [25, 30, 35],
+        'City': ['New York', 'Los Angeles', 'Chicago']
+    }
+    df = pd.DataFrame(data)
+    df.to_csv('/opt/airflow/dags/data/output.csv', index=False)
+
+
+def create_schema():
+    client.timeout = 3000
+    client.execute("""CREATE DATABASE IF NOT EXISTS raw""")
+
+
+def create_table():
+    client.timeout = 3000
+    client.execute("""
+        CREATE TABLE IF NOT EXISTS raw.my_table (
+            id UInt64,
+            name String,
+            dt Date
+            )
+        ENGINE = MergeTree
+        ORDER BY (id, dt)
+        PARTITION BY toYYYYMM(dt)
+        """)
+
+def insert_into_table():
+    client.timeout = 3000
+    client.execute("""
+        INSERT INTO raw.my_table (id, name, dt)
+        SELECT
+            number AS id,
+            arrayJoin(['Alice', 'Bob', 'Charlie', 'David', 'Eve']) AS name,
+            toDate(toStartOfHour(now()) - number * 60 * 60) AS dt
+        FROM numbers(1000)
+        """)
+
+stage1 = PythonOperator(
+    task_id="get_data_from_api",
+    python_callable=get_data_from_api,
+    dag=dag
+)
+
+stage2 = PythonOperator(
+    task_id="create_schema",
+    python_callable=create_schema,
+    dag=dag
+)
+
+stage3 = PythonOperator(
+    task_id="create_table",
+    python_callable=create_table,
+    dag=dag
+)
+
+stage4 = PythonOperator(
+    task_id="insert_into_table",
+    python_callable=insert_into_table,
+    dag=dag
+)
+
+stage1 >> stage2 >> stage3 >> stage4
